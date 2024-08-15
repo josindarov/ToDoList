edf23f5eb0b8c91ad7be8871214a7c707d669984
Jurabek <jorabeksindarov44@gmail.com>
2024-08-15 12:35:22 +0500

    Task repository methods implemented

--- ToDoList/app/Repository/TaskRepository.php
+++ ToDoList/app/Repository/TaskRepository.php
@@ -2,33 +2,40 @@
 
 namespace App\Repository;
 
+use App\Models\Task;
 use App\Repository\ITaskRepository;
 
 class TaskRepository implements ITaskRepository
 {
-
+    private Task $task;
+    function __construct(Task $task)
+    {
+        $this->$task = $task;
+    }
     public function GetAllTasks()
     {
-        // TODO: Implement GetAllTasks() method.
+        return $this->task->all();
     }
 
     public function GetTaskById($id)
     {
-        // TODO: Implement GetTaskById() method.
+        return $this->task->findOrFail($id);
     }
 
     public function AddTask($task)
     {
-        // TODO: Implement AddTask() method.
+        return $this->task->create($task);
     }
 
     public function DeleteTask($id)
     {
-        // TODO: Implement DeleteTask() method.
+        return $this->task->findOrFail($id)->delete();
     }
 
     public function UpdateTask($id, $task)
     {
-        // TODO: Implement UpdateTask() method.
+        $updatedTask = $this->GetTaskById($id);
+        $updatedTask->update($task);
+        return $updatedTask;
     }
 }
